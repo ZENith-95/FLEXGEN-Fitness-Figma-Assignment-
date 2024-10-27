0cb5168d1a1c1f315abb037f9aa529e67669cd00
ZENith-95 <dz.korbla@gmail.com>
2024-10-27 16:57:33 +0000

    First commit for FLEXGEN Fitness assignment from UI UX design class

---
+++ script.js
@@ -0,0 +1,8 @@
+function showDropdown() {
+  const dropdown = document.querySelector(".dropdown-nav");
+  dropdown.style.display = "flex";
+}
+function hideDropdown() {
+  const dropdown = document.querySelector(".dropdown-nav");
+  dropdown.style.display = "none";
+}
